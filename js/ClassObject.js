b446e570db8aff0f7b7e0660948f6f9c36a6a073
inker610566 <inker610566@yahoo.com.tw>
2015-12-04 12:40:44 +0800

    Protyping of add line dragging

--- js/ClassObject.js
+++ js/ClassObject.js
@@ -7,5 +7,27 @@ var ClassObject = function(sx, sy)
         ChangeXY: do_nothing, // (x, y) -> ()
         Focus: do_nothing,
         UnFocus: do_nothing
+    };
+    var w = 50, h = 50;
+    function InPort0(x, y) { return -y >= Math.abs( x); }
+    function InPort1(x, y) { return -x >= Math.abs( y); }
+    function InPort2(x, y) { return  y >= Math.abs( x); }
+    function InPort3(x, y) { return  x >= Math.abs( y); }
+    var port_offset = [[0, -h/2],[-w/2, 0],[0, h/2],[w/2, 0]];
+    var port_check = [InPort0, InPort1, InPort2, InPort3];
+    // return 5 if not in any port else port no
+    this.CheckInPort = function(x, y)
+    {
+        x -= w/2;
+        y -= h/2;
+        for(var i = 0 ; i < 4 ; i ++)
+            if(port_check[i](x, y))
+                return i;
+        return 5;
+    };
+    this.PortLoc = function(no)
+    {
+        return [this.x+w/2+port_offset[no][0],
+                this.y+h/2+port_offset[no][1]];
     }
 };
--- js/mode/AddLineMode.js
+++ js/mode/AddLineMode.js
@@ -1,20 +1,57 @@
 var AddLineMode = function(model)
 {
-    var _selecting = null,
+    var _startDragging = null,
         _draggingLine = null;
     this.MouseDownObject = function(_class, x, y)
     {
+        if(_startDragging != null)
+        {
+            // assert false
+        }
+        else
+        {
+            _startDragging = _class;
+            _class.UIReflectors.Focus();
+            // find start port
+            var port_no = _class.CheckInPort(x, y);
+            // assert port_no != 5
+            var port_loc = _class.PortLoc(port_no);
+            _draggingLine = new LineObject(
+                port_loc[0], port_loc[1], x, y
+            );
+            model.Events.OnNewLine(_draggingLine);
+        }
     };
     this.MouseUpObject = function(_class, x, y)
     {
-        x += _class.x;
-        y += _class.y;
+        if(_draggingLine != null)
+        {
+            // assert _startDragging != null
+            if(_startDragging != _class)
+            {
+                var port_no = _class.CheckInPort(x, y);
+                // assert port_no != 5
+                var port_loc = _class.PortLoc(port_no);
+                _draggingLine.endpoints[1] = port_loc;
+                _draggingLine.UIReflectors.ChangeXY(
+                    _draggingLine.endpoints
+                );
+                _startDragging = _draggingLine = null;
+            }
+            else
+                ;// cancel
+        }
     };
     this.MouseMoveCanvas = function(x, y)
     {
-        line.endpoints[1][0] = x;
-        line.endpoints[1][1] = y;
-        line.UIReflectors.ChangeXY(line.endpoints);
+        if(_draggingLine != null)
+        {
+            _draggingLine.endpoints[1][0] = x;
+            _draggingLine.endpoints[1][1] = y;
+            _draggingLine.UIReflectors.ChangeXY(
+                _draggingLine.endpoints
+            );
+        }
     };
     this.MouseLeaveCanvas = function(x, y)
     {
@@ -25,10 +62,8 @@ var AddLineMode = function(model)
     this.MouseUpCanvas = function(x, y)
     {
     };
-    var line = new LineObject(100, 100, 200, 200);
     this.EnterMode = function()
     {
-        model.Events.OnNewLine(line);
     };
     this.LeaveMode = function()
     {
